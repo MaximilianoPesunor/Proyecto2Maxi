9bff29fa6dd118d359452269230bfab1e80ef0cc
FlutLab.io <>
2022-06-23 23:44:43 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/proyectoapimaximiliano/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proyectoapimaximiliano
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: 'Wikipedia search API'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future getAutoData() async {
+    var response = await http.get(Uri.parse("https://xkcd.com/info.0.json"));
+
+    var jsonData = jsonDecode(response.body);
+
+    List<Comic> comics = [];
+
+    for (var u in jsonData) {
+      Comic comic = Comic(u["month"], u["num"], u["link"], u["year"], u["news"], u["safe_title"], u["transcript"], u["alt"], u["img"], u["title"], u["day"]);
+      comics.add(comic);
+    }
+    return comics;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Proyecto 2 Maximiliano'),
+      ),
+      body: Container(
+        child: Card(
+          child: FutureBuilder(
+            future: getAutoData(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
+                return Container(
+                  child: Center(child: Text('CARGANDO API')),
+                );
+              } else {
+                return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, i) {
+                      return ListTile(
+                        title: Text(snapshot.data[i].id),
+                      );
+                    });
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Comic {
+  final String month, number, link, year, news, safe_title, transcript, alt, img, title, day;
+  Comic(this.month, this.number, this.link, this.year, this.news, this.safe_title, this.transcript, this.alt, this.img, this.title, this.day);
+}
